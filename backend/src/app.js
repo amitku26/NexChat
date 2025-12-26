dd0dde7f7341414a27ebdb7ad065254dd6df5b24
amitkumar2026 <amitkumar829448@gmail.com>
2025-12-26 16:29:20 +0530

    error fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,38 +1,5 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import { app, server } from "./lib/socket.js";
-
-// dotenv.config();
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-
-// const port = process.env.PORT;
-
-// import authRoute from "./routes/authRoute.js";
-// import messageRoute from "./routes/messageRoute.js";
-
-// app.use("/api/auth", authRoute);
-// app.use("/api/message", messageRoute);
-
-// mongoose.connect(process.env.MONGODB_URI).then((res) => {
-//   console.log("mongoDB connected:" + res.connection.host);
-//   server.listen(port, () => {
-//     console.log(`server running on port ${port}`);
-//   });
-// });
-
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -47,7 +14,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "nexchat-cyan.vercel.app",
     credentials: true,
   })
 );
